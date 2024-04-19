f2773cb33be435e008c4ba66d32ff8925d9669aa
gvbonifacio <bonifacio.gio@gmail.com>
2024-04-18 22:47:53 -0300

    Ajustes para utilizar mongodb

--- main.py
+++ main.py
@@ -1,124 +1,49 @@
 import requests
-import csv
+from requests import exceptions
 import ssl
+import json
+from googlesearch import search
+from pymongo import MongoClient
+from utils import (possiveisUrlsPorNomeTratado, trataCaracteresEspeciaisGlobal,
+                   trataCaracteresEspeciaisComTraco)
+
+# Conecte-se ao MongoDB (ajuste o host e a porta conforme necessário)
+client = MongoClient("mongodb://localhost:27017/")
+
+# Escolha o banco de dados e a coleção
+db = client["local"]
+collection = db["municipiosTcc"]
 
 
-# CONSTANTES
-SIGLA_ESTADO_COLUMN = 4
-NOME_MUNICIPIO_COLUMN = 2
+def verificaSeMunicipioPossuiPortalPrefeitura():
+    nomeMunicipioOriginal = municipio.get('municipioTOM').lower()
+    estado = municipio.get('uf').lower()
+    rowId = municipio.get('_id')
 
-##GLOBAIS
-municipiosStatus200MasNaoIdentificouHtml = []
-municipiosStatus200MasErroDecoding = []
-municipiosLogoGovCidades = []
-municipiosErroTimeout = []
-municipiosComPortal503Manutencao = []
+    nomeMunicipioBuscaGoogle = nomeMunicipioOriginal.replace(' ', '+')
 
+    urlBuscaGoogle = f'prefeitura+municipal+de+{nomeMunicipioBuscaGoogle}+{estado}'
 
-# URL_PADRAO = "http://www.maringa.pr.gov.br"
+    print(urlBuscaGoogle)
+    resultadoBuscaGoogle = google(urlBuscaGoogle)
 
+    listaLinksResultadoGoogle = []
 
-def existePortalPrefeituraParaUrl(url):
-    try:
-        headers = {
-            'User-Agent': 'My User Agent 1.0',
-            'From': 'youremail@domain.example'  # This is another valid field
-        }
+    for link in resultadoBuscaGoogle:
+        print("Link Google :" + link)
+        listaLinksResultadoGoogle.append(link)
 
-        response = requests.get(url,headers=headers, verify=False, timeout=3, allow_redirects=True)
-        print(response)
-        print(response.status_code)
+        if urlFormatoEsperadoParaPortalDePrefeitura(estado, link):
 
-        if response.status_code == 503 and ('manutenção' or 'manutencao') in response.text.lower() :
-            print("503 em manutenção")
-            municipiosComPortal503Manutencao.append(municipio)
-            return True
+            print("Link Google padrao pref:" + link)
+            if consegueAcessarPortalPrefeituraParaUrl(link):
+                print("existePortalPrefeituraParaUrl")
+                collection.update_one({'_id': rowId}, {"$set": municipio}, upsert=False)
+                return True
 
+    print("não identifica com busca google, tentando acessar possiveis dominios")
 
-        if response.status_code != 200:
-            print("chamada não ok")
-            return False
-
-
-        municipiosStatus200MasErroDecoding.append(municipio)
-
-        responseText = response.text
-
-        municipiosStatus200MasErroDecoding.remove(municipio)
-
-        return htmlRepresentaPortalPrefeitura(responseText)
-    except Exception as e:
-        print(e)
-        return False
-
-
-def htmlRepresentaPortalPrefeitura(texto):
-    nomeOriginal = municipio[NOME_MUNICIPIO_COLUMN].lower()
-    nomeTratadoSemEspaco = trataCaracteresEspeciaisGlobal(nomeOriginal)
-    nomeTratadoComTraco = trataCaracteresEspeciaisComTraco(nomeOriginal)
-    if 'prefeitura' or nomeOriginal or nomeTratadoComTraco or nomeTratadoSemEspaco in texto.lower():
-        if 'logo-govcidades' in texto.lower():
-            municipiosLogoGovCidades.append(municipio)
-        return True
-    else:
-        municipiosStatus200MasNaoIdentificouHtml.append(municipio)
-        return False
-
-
-def trataCaracteresEspeciaisGlobal(nome):
-    nome = nome.lower()
-    nome = nome.replace('ã', 'a')
-    nome = nome.replace('á', 'a')
-    nome = nome.replace('à', 'a')
-    nome = nome.replace('â', 'a')
-    nome = nome.replace('ã', 'a')
-    nome = nome.replace('é', 'e')
-    nome = nome.replace('ê', 'e')
-    nome = nome.replace('í', 'i')
-    nome = nome.replace('ì', 'i')
-    nome = nome.replace('ó', 'o')
-    nome = nome.replace('õ', 'o')
-    nome = nome.replace('ô', 'o')
-    nome = nome.replace('ú', 'u')
-    nome = nome.replace('ù', 'u')
-    nome = nome.replace('ç', 'c')
-    nome = nome.replace('d\'a', 'da a')
-    nome = nome.replace('d\'o', 'do o')
-    nome = nome.replace('d\'e', 'de e')
-    nome = nome.replace('-', '')
-    nome = nome.replace(' ', '')
-    return nome
-
-
-def trataCaracteresEspeciaisComTraco(nome):
-    nome = nome.lower()
-    nome = nome.replace('ã', 'a')
-    nome = nome.replace('á', 'a')
-    nome = nome.replace('à', 'a')
-    nome = nome.replace('â', 'a')
-    nome = nome.replace('ã', 'a')
-    nome = nome.replace('é', 'e')
-    nome = nome.replace('ê', 'e')
-    nome = nome.replace('í', 'i')
-    nome = nome.replace('ì', 'i')
-    nome = nome.replace('ó', 'o')
-    nome = nome.replace('õ', 'o')
-    nome = nome.replace('ô', 'o')
-    nome = nome.replace('ú', 'u')
-    nome = nome.replace('ù', 'u')
-    nome = nome.replace('ç', 'c')
-    nome = nome.replace('d\'a', 'da a')
-    nome = nome.replace('d\'o', 'do o')
-    nome = nome.replace('d\'e', 'de e')
-    nome = nome.replace(' ', '-')
-    return nome
-
-
-def municipioPossuiPortalPrefeitura():
-    nomeMunicipioOriginal = municipio[NOME_MUNICIPIO_COLUMN]
     nomeMunicipioTratado = trataCaracteresEspeciaisGlobal(nomeMunicipioOriginal)
-
-    estado = municipio[SIGLA_ESTADO_COLUMN].lower()
     listaDePossiveisURLs = possiveisUrlsPorNomeTratado(estado, nomeMunicipioTratado)
 
     if '-' or ' ' in nomeMunicipioOriginal:
@@ -128,67 +53,86 @@ def municipioPossuiPortalPrefeitura():
             listaDePossiveisURLs.append(url)
 
     for possivelUrl in listaDePossiveisURLs:
+        listaLinksResultadoGoogle.append(possivelUrl)
         print(possivelUrl)
 
-        if existePortalPrefeituraParaUrl(possivelUrl):
+        if consegueAcessarPortalPrefeituraParaUrl(possivelUrl):
             print("existePortalPrefeituraParaUrl")
+            collection.update_one({'_id': rowId}, {"$set": municipio}, upsert=False)
             return True
 
     return False
 
 
-def possiveisUrlsPorNomeTratado(estado, nomeTratado):
-    return [
-        f'https://www.{nomeTratado}.{estado}.gov.br',
-        f'http://www.{nomeTratado}.{estado}.gov.br',
-        f'https://{nomeTratado}.{estado}.gov.br',
-        f'https://{nomeTratado}.{estado}.gov.br',
-        f'www.{nomeTratado}.{estado}.gov.br'
-    ]
+def urlFormatoEsperadoParaPortalDePrefeitura(estado, link):
+    return (".gov.br" in link or ".atende.net" in link) and (
+            f'tcm.{estado}.gov.br' not in link
+            and 'datasus.gov.br' not in link
+            and 'portaldatransparencia.gov.br' not in link
+            and 'ibge.' not in link
+            and 'diariooficial' not in link
+            and 'comprasnet.gov.br' not in link
+            and 'defesacivil.' not in link
+            and f'tce.{estado}.gov.br' not in link
+    )
+
+
+def google(query):
+    return search(query, num_results=10, lang="pt-br")
+
+
+def consegueAcessarPortalPrefeituraParaUrl(url):
+    try:
+        headers = {
+            'User-Agent': 'My User Agent 1.0',
+            'From': 'youremail@domain.example'  # This is another valid field
+        }
+
+        response = requests.get(url, headers=headers, verify=False, timeout=15, allow_redirects=True)
+
+        print(response)
+        print(response.status_code)
+
+        if response.status_code == 503 and ('manutenção' or 'manutencao') in response.text.lower():
+            print("503 em manutenção")
+            municipio['emManutencao'] = True
+
+        if response.status_code != 200:
+            municipio['identificouConteudoHtml'] = False
+        else:
+            responseText = response.text.lower()
+            municipio['logoGovCidadesBr'] = pertenceAoPortalCidadesGovBr(responseText)
+            municipio['identificouConteudoHtml'] = interpretaHtmlRepresentaPortalPrefeitura(responseText)
+
+        municipio['url'] = url
+        return True
+    except exceptions.RetryError as retryError:
+        print(retryError)
+        return False
+    except Exception as e:
+        print(e)
+        return False
+
+
+def interpretaHtmlRepresentaPortalPrefeitura(texto):
+    nomeOriginal = municipio["municipioTOM"].lower()
+    nomeTratadoSemEspaco = trataCaracteresEspeciaisGlobal(nomeOriginal)
+    nomeTratadoComTraco = trataCaracteresEspeciaisComTraco(nomeOriginal)
+    nomeTratadoTracoCharEspecial = nomeOriginal.replace('\'', '’')
+
+    return 'prefeitura' in texto and (
+            nomeOriginal in texto or nomeTratadoComTraco in texto or nomeTratadoSemEspaco in texto or nomeTratadoTracoCharEspecial in texto)
+
+
+def pertenceAoPortalCidadesGovBr(texto):
+    return 'govbr-cidades-footer' in texto
 
 
 if __name__ == '__main__':
     ssl._create_default_https_context = ssl._create_stdlib_context
 
-    with open('municipios.csv', mode='r', encoding='utf-8') as planilhaDeMunicipios:
-        listaDeMunicipios = csv.reader(planilhaDeMunicipios, delimiter=';')
-
-        municipiosComPortalPrefeitura = []
-        municipiosSemPortalPrefeitura = []
-
-        for municipio in listaDeMunicipios:
-            if municipioPossuiPortalPrefeitura():
-                municipiosComPortalPrefeitura.append(municipio)
-            else:
-                municipiosSemPortalPrefeitura.append(municipio)
-
-    print("municipiosComPortalPrefeitura")
-    print(municipiosComPortalPrefeitura)
-    print(
-        "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
-    print("municipiosSemPortalPrefeitura")
-    print(municipiosSemPortalPrefeitura)
-    print(
-        "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
-    print("municipiosStatus200MasErroDecoding")
-    print(municipiosStatus200MasErroDecoding)
-    print(
-        "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
-    print("municipiosStatus200MasNaoIdentificouHtml")
-    print(municipiosStatus200MasNaoIdentificouHtml)
-
-    print(
-        "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
-    print("municipiosErroTimeout")
-    print(municipiosErroTimeout)
-
-    print(
-        "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
-    print("municipiosLogoGovCidades")
-    print(municipiosLogoGovCidades)
-
-
-    print(
-        "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
-    print("municipiosComPortal503Manutencao")
-    print(municipiosComPortal503Manutencao)
+    query = {'url': {'$exists': False}}
+    for municipio in collection.find(query):
+        # for municipio in collection.find():
+        print(municipio)
+        verificaSeMunicipioPossuiPortalPrefeitura()
